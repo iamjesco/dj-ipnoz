9de43ae29858d8ef784236ad2714ca4da92dabcb
iamjesco <jesco1979@hotmail.com>
2020-08-27 00:11:37 -0400

    Added sections and music

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,25 @@
 // Call the scrollchange function when the user scrolls
-window.addEventListener('scroll', function () {
-	scrollChange();
-});
+// window.addEventListener('scroll', function () {
+// 	scrollChange();
+// });
 
 
-function scrollChange() {
-	const header = document.getElementById('nav');
-	// Add the class 'fade-in' after scrolling to more than 50
-	if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50){
-		header.classList.add('fade-in');
-		// Remove the class 'fade-in' after scrolling to less than 50
-	}else {
-		header.classList.remove('fade-in');
-	}
-}
+// function scrollChange() {
+// 	const header = document.getElementById('nav');
+// 	// Add the class 'fade-in' after scrolling to more than 50
+// 	if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50){
+// 		header.classList.add('fade-in');
+// 		// Remove the class 'fade-in' after scrolling to less than 50
+// 	}else {
+// 		header.classList.remove('fade-in');
+// 	}
+// }
+
+const parallax = document.getElementById('hero');
+
+window.addEventListener("scroll", function(){
+  let offset = window.pageYOffset;
+  parallax.style.backgroundPositionY = offset * -.4 + "px";
+  // console.log(`offset: ${offset}`);
+  // console.log(`offset * 0.7: ${offset * 0.7}`);
+})
