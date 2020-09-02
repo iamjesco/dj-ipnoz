352aa6b89cc72da56aa8ad1fde6987efca52131a
iamjesco <jesco1979@hotmail.com>
2020-09-01 23:25:49 -0400

    Added images, text and sections

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,21 +1,21 @@
 // Call the scrollchange function when the user scrolls
-// window.addEventListener('scroll', function () {
-// 	scrollChange();
-// });
+window.addEventListener('scroll', function () {
+	scrollChange();
+});
 
 
-// function scrollChange() {
-// 	const header = document.getElementById('nav');
-// 	// Add the class 'fade-in' after scrolling to more than 50
-// 	if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50){
-// 		header.classList.add('fade-in');
-// 		// Remove the class 'fade-in' after scrolling to less than 50
-// 	}else {
-// 		header.classList.remove('fade-in');
-// 	}
-// }
+function scrollChange() {
+	const header = document.getElementById('header');
+	// Add the class 'fade-in' after scrolling to more than 50
+	if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50){
+		header.classList.add('fade-in');
+		// Remove the class 'fade-in' after scrolling to less than 50
+	}else {
+		header.classList.remove('fade-in');
+	}
+}
 
-const parallax = document.getElementById('hero');
+const parallax = document.getElementById('home');
 
 window.addEventListener("scroll", function(){
   let offset = window.pageYOffset;
