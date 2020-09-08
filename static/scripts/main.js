fdddefd8c8b5d1519b02ef6553a53fbbc23d42d4
iamjesco <jesco1979@hotmail.com>
2020-09-08 14:41:35 -0400

    Finished rough website draft

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -4,16 +4,16 @@ window.addEventListener('scroll', function () {
 });
 
 
-function scrollChange() {
-	const header = document.getElementById('header');
-	// Add the class 'fade-in' after scrolling to more than 50
-	if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50){
-		header.classList.add('fade-in');
-		// Remove the class 'fade-in' after scrolling to less than 50
-	}else {
-		header.classList.remove('fade-in');
-	}
-}
+// function scrollChange() {
+// 	const header = document.getElementById('header');
+// 	// Add the class 'fade-in' after scrolling to more than 50
+// 	if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50){
+// 		header.classList.add('fade-in');
+// 		// Remove the class 'fade-in' after scrolling to less than 50
+// 	}else {
+// 		header.classList.remove('fade-in');
+// 	}
+// }
 
 const parallax = document.getElementById('home');
 
@@ -23,3 +23,18 @@ window.addEventListener("scroll", function(){
   // console.log(`offset: ${offset}`);
   // console.log(`offset * 0.7: ${offset * 0.7}`);
 })
+
+
+// FUNCTION TO ACTIVATE BURGER MENU
+document.getElementById("bars").addEventListener("click", openNav);
+
+document.getElementById("cross").addEventListener("click", closeNav);
+
+function openNav() {
+  document.getElementById("navigation_sm").style.cssText = "animation: open .5s forwards;";
+}
+
+function closeNav() {
+  document.getElementById("navigation_sm").style.cssText = "animation: close .5s forwards;";
+}
+
